05996d407b765e523e82ecf5a9eac14082fa3eb2
Theodor Coelho <theodorfcoelho@gmail.com>
2024-05-01 08:42:50 -0300

    Inseridas novas verificações dos campos

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,6 +5,18 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.visit('./src/index.html')
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+
+        cy.get('#firstName')
+        .type('Theodor')
+        .should('have.value', 'Theodor')
+        
+        cy.get('#email')
+        .type('theodorfcoelho@gmail.com')
+        .should('have.value', 'theodorfcoelho@gmail.com')
+        
+        cy.get('.button').click()
+
+
     })
   })
   
