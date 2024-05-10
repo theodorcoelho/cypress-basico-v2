423164b7c2cfdcb6f07f1103a4823131de93096f
Theodor Coelho <theodorfcoelho@gmail.com>
2024-05-10 18:32:47 -0300

    Validando se os campos estão visíveis

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,22 +1,34 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    it('Verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
 
         cy.get('#firstName')
+        .should('be.visible')
         .type('Theodor')
         .should('have.value', 'Theodor')
+
+        cy.get('#lastName')
+        .should('be.visible')
+        .type('Coelho')
+        .should('have.value','Coelho')
         
         cy.get('#email')
+        .should('be.visible')
         .type('theodorfcoelho@gmail.com')
         .should('have.value', 'theodorfcoelho@gmail.com')
+
+        cy.get('#open-text-area')
+        .should('be.visible')
+        .type('CAMPO TEXTO')
+        .should('have.value', 'CAMPO TEXTO')
+
+ 
         
         cy.get('.button').click()
-
-
     })
   })
   
