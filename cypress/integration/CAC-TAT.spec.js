17759f6c3a1a2c8323bb8ab70d8569b4e23c240c
Theodor Coelho <theodorfcoelho@gmail.com>
2024-05-11 13:34:20 -0300

    ajustes no código e realização do exercício 7

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,96 +1,96 @@
 /// <reference types="Cypress" />
 
-var text = String
-text = "Sed a bibendum lacus. Pellentesque in bibendum diam. Aenean ultricies in leo non pretium. Integer eu efficitur dui. Pellentesque hendrerit interdum orci sed maximus. Proin quam quam, ultricies at enim in, tristique tincidunt nibh. Vestibulum dictum mauris quis quam aliquam dapibus. Quisque vitae felis placerat, vulputate ex sed, sagittis lectus. Integer malesuada sapien a libero elementum interdum."
+const text = "Sed a bibendum lacus. Pellentesque in bibendum diam. Aenean ultricies in leo non pretium. Integer eu efficitur dui. Pellentesque hendrerit interdum orci sed maximus. Proin quam quam, ultricies at enim in, tristique tincidunt nibh. Vestibulum dictum mauris quis quam aliquam dapibus. Quisque vitae felis placerat, vulputate ex sed, sagittis lectus. Integer malesuada sapien a libero elementum interdum."
 
 describe('Central de Atendimento ao Cliente TAT', function() {
     beforeEach(() => {
         cy.visit('./src/index.html')
     })
-
+    // é possível usar it.only() para rodar apenas aquele teste em específico
     it('verifica o título da aplicação', function() {
-
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-
     })
 
     it('preenche os campos obrigatórios e envia o formulário', function() {
 
         cy.get('#firstName')
-        .should('be.visible')
-        .type('Theodor',{delay:0})
-        .should('have.value', 'Theodor')
+          .should('be.visible')
+          .type('Theodor',{delay:0})
+          .should('have.value', 'Theodor')
 
         cy.get('#lastName')
-        .should('be.visible')
-        .type('Coelho',{delay:0})
-        .should('have.value','Coelho')
+          .should('be.visible')
+          .type('Coelho',{delay:0})
+          .should('have.value','Coelho')
         
         cy.get('#email')
-        .should('be.visible')
-        .type('theodorfcoelho@gmail.com',{delay:0})
-        .should('have.value', 'theodorfcoelho@gmail.com')
+          .should('be.visible')
+          .type('theodorfcoelho@gmail.com',{delay:0})
+          .should('have.value', 'theodorfcoelho@gmail.com')
 
         cy.get('#open-text-area')
-        .should('be.visible')
-        .type(text,{delay:0})
-        .should('have.value', text)
-        
-        cy.get('.button').click()
+          .should('be.visible')
+         .type(text,{delay:0})
+         .should('have.value', text)
+          
+        cy.get('button[type="submit"]').click()
 
         cy.get('.success')
-        .should('be.visible')
+          .should('be.visible')
     })
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
         cy.get('#firstName')
-        .should('be.visible')
-        .type('Theodor',{delay:0})
-        .should('have.value', 'Theodor')
-
+          .should('be.visible')
+          .type('Theodor',{delay:0})
+          .should('have.value', 'Theodor')
         cy.get('#lastName')
-        .should('be.visible')
-        .type('Lupin',{delay:0})
-        .should('have.value','Lupin')
-        
+          .should('be.visible')
+          .type('Lupin',{delay:0})
+          .should('have.value','Lupin')
         cy.get('#email')
-        .should('be.visible')
-        .type('theodorfcoelhogmail.com',{delay:0})
-        .should('have.value','theodorfcoelhogmail.com')
-
+          .should('be.visible')
+          .type('theodorfcoelhogmail.com',{delay:0})
+          .should('have.value','theodorfcoelhogmail.com')
         cy.get('#open-text-area')
-        .should('be.visible')
-        .type(text,{delay:0})
-        .should('have.value',text)
+          .should('be.visible')
+          .type(text,{delay:0})
+          .should('have.value',text)
 
-        cy.get('.button').click()
+        cy.get('button[type="submit"]').click()
 
-        cy.get('.error')
-        .should('be.visible')
+        cy.get('.error').should('be.visible')
 
     })
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function() {
+        cy.get('#phone')
+          .type('abcde')
+          .should('have.value','')
+    })
+
+
     it ('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
         cy.get('#firstName')
-        .should('be.visible')
-        .type('Theodor',{delay:0})
-        .should('have.value', 'Theodor')
+          .should('be.visible')
+          .type('Theodor',{delay:0})
+          .should('have.value', 'Theodor')
 
         cy.get('#lastName')
-        .should('be.visible')
-        .type('Lupin',{delay:0})
-        .should('have.value','Lupin')
+          .should('be.visible')
+          .type('Lupin',{delay:0})
+          .should('have.value','Lupin')
         
         cy.get('#email')
-        .should('be.visible')
-        .type('theodorfcoelhogmail.com',{delay:0})
-        .should('have.value','theodorfcoelhogmail.com')
+          .should('be.visible')
+          .type('theodorfcoelho@gmail.com',{delay:0})
+          .should('have.value','theodorfcoelho@gmail.com')
 
         cy.get('#open-text-area')
-        .should('be.visible')
-        .type(text,{delay:0})
-        .should('have.value',text)
+          .should('be.visible')
+          .type(text,{delay:0})
+          .should('have.value',text)
 
         cy.get('#phone-checkbox').click()
-        cy.get('.button').click()
+        cy.get('button[type="submit"]').click()
         cy.get('.error').should('be.visible')
     })
     it ('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
@@ -99,33 +99,44 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .type('Theodor',{delay:0})
         .should('have.value', 'Theodor')
         .clear()
+        .should('have.value','')
 
         cy.get('#lastName')
         .should('be.visible')
         .type('Lupin',{delay:0})
         .should('have.value','Lupin')
         .clear()
+        .should('have.value','')
         
         cy.get('#email')
         .should('be.visible')
-        .type('theodorfcoelhogmail.com',{delay:0})
-        .should('have.value','theodorfcoelhogmail.com')
+        .type('theodorfcoelho@gmail.com',{delay:0})
+        .should('have.value','theodorfcoelho@gmail.com')
         .clear()
+        .should('have.value','')
 
         cy.get('#phone')
         .should('be.visible')
         .type('48988402322',{delay:0})
         .clear()
+        .should('have.value','')
 
         cy.get('#open-text-area')
         .should('be.visible')
         .type(text,{delay:0})
         .should('have.value',text)
         .clear()       
+        .should('have.value','')
     })
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
-        cy.get('.button').click()
+        cy.get('button[type="submit"]').click()
         cy.get('.error').should('be.visible')
+    })
+    it('envia o formuário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit(text)
+        
+        cy.get('.success')
+          .should('be.visible')
     })
 
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,31 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(text) {
+    cy.get('#firstName')
+          .should('be.visible')
+          .type('Theodor',{delay:0})
+          .should('have.value', 'Theodor')
+
+        cy.get('#lastName')
+          .should('be.visible')
+          .type('Coelho',{delay:0})
+          .should('have.value','Coelho')
+        
+        cy.get('#email')
+          .should('be.visible')
+          .type('theodorfcoelho@gmail.com',{delay:0})
+          .should('have.value', 'theodorfcoelho@gmail.com')
+
+        cy.get('#open-text-area')
+          .should('be.visible')
+         .type(text,{delay:0})
+         .should('have.value', text)
+          
+        cy.get('button[type="submit"]').click()
+})
+
+
+
+
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
