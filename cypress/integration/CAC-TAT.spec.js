2d7cb92cad9bd841acf3e619c02a4b96b2b4c3b1
Theodor Coelho <theodorfcoelho@gmail.com>
2024-07-30 21:56:02 -0300

    lesson 7

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -219,7 +219,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           expect($input[0].files[0].name).to.equal('example.json')
         })
     })  
-    it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
       cy.fixture('example.json').as('exampleFile')
       
       cy.get('input[type="file"]#file-upload')
@@ -228,5 +228,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
           expect($input[0].files[0].name).to.equal('example.json')
         })
     })
+    it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function() {
+      cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', function() {
+      cy.get('#privacy a')
+        .invoke('removeAttr', 'target')
+        .click()
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+    })
+    
   })
   
---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,4 @@
+it('testa a página da política de privacidade de forma independente', function() {
+    cy.visit('/src/privacy.html')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+  }) 
